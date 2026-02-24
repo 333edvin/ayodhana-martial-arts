744a9d776c116a58a81c1a71064042bdb13ec320
333edvin <edvinjohnson333@gmail.com>
2026-02-24 11:43:36 +0530

    favicon

--- app/layout.js
+++ app/layout.js
@@ -13,10 +13,19 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Ayodhana Fighters",
-  description: "Master discipline and strength at Ayodhana Martial Arts Academy. Expert training in Karate, Kickboxing, and Self-Defense for all ages. Join the fight today!",
-};
-
+  title: 'Ayodhana Fighters',
+  description: 'Master discipline and strength at Ayodhana Martial Arts Academy. Expert training in Karate, Kickboxing, and Self-Defense for all ages. Join the fight today!',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        type: 'image/x-icon',
+      },
+    ],
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
+}
 
 
 export default function RootLayout({ children }) {
